e2238a47a097e11fc3081374879dde49bfc569f8
Amanda <amandabaydar@gmail.com>
2021-10-06 15:38:47 -0400

    try to connect to postgresql db

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ dotenv.config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const mongoose = require('mongoose')
 const boardgamesRoutes = require('./routes/boardgames')
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
+const {getBoardgames} = require('./queries')
 
 
 
 const port = 3000
 
 app.listen(port, () => {console.log(`listening on ${port}`)})
+
+app.use(bodyParser.json())
+
+app.get('/', () => getBoardgames)
+
 
