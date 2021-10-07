48703a7d118fde773c08fa5e881e5edcbe709c50
Amanda <amandabaydar@gmail.com>
2021-10-06 20:02:07 -0400

    add cors to allow api call from front end

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ const mongoose = require('mongoose')
 const boardgamesRoutes = require('./routes/boardgames')
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
+const cors = require('cors');
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('db connected'))
 
 mongoose.connection.on('error', err => console.log(`DB connection error: ${err.message}`))
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(expressValidator())
 app.use('/', boardgamesRoutes)
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {console.log(`listening on ${port}`)})
+app.listen(port, () => {console.log(`A node js api is listening on port ${port}`)})
